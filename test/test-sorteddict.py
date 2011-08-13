1a29b6a55a8af23005aa29e830c79c05e4b60e4b
moliware <miguel@moliware.com>
2011-08-13 09:18:36 +0200

    Added first test case for SortedDict

---
+++ test/test-sorteddict.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from dicts import SortedDict
+
+
+
+class SortedDictTestCase(unittest.TestCase):
+    """SortedDict test cases."""
+
+    def setUp(self):
+        self.sd = SortedDict({'c' : (1,'b'), 'a' : (6,'c'), 'b' : (5,'a')},
+                             key=lambda x: x[1][1])
+
+    def tearDown(self):
+        pass
+
+    def test_items(self):
+        self.assertEqual(self.sd.items(),
+                         [('b', (5, 'a')), ('c', (1, 'b')), ('a', (6, 'c'))])
+
+    def test_keys(self):
+        self.assertEqual(self.sd.keys(), ['b', 'c', 'a'])
+
+    def test_values(self):
+        self.assertEqual(self.sd.values(), [(5, 'a'), (1, 'b'), (6, 'c')])
+
+if __name__ == '__main__':
+    unittest.main()
